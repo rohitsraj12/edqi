10b7d8bb1ac5d2c9e552bf839635d5c86efa69fb
Rohit <rohitsraj12@gmail.com>
2021-11-09 16:09:40 +0530

    Create header.inc.php

---
+++ website/private/required/header.inc.php
@@ -0,0 +1,55 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $page_title; ?> | EDQI.com</title>
+    <meta name="description" content="">
+    <meta name="keywords" content="<?php echo $page_meta_key; ?>">
+    <meta name="author" content="<?php echo $page_meta_description; ?>">
+    <meta name="distribution" content="global" />
+    <meta http-equiv="content-language" content="en-gb">
+    <meta name="city" content="bangalore" />
+    <meta name="geo.region" content="IN" />
+    <meta name="state" content="Karnataka" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:creator" content="@"/>
+    <meta name="twitter:description" content="" />
+    <meta name="twitter:keywords" content="">
+    <!-- open graph code begins-->
+    <meta property="og:type" content="business.business">
+    <meta property="og:title" content=" ">
+    <meta property="og:url" content="">
+    <meta property="business:contact_data:street_address" content="">
+    <meta property="business:contact_data:locality" content="Bangalore">
+    <meta property="business:contact_data:region" content="Karnataka">
+    <meta property="business:contact_data:postal_code" content="">
+    <meta property="business:contact_data:country_name" content="India">
+    <!-- google -->
+    <meta name="google-site-verification" content="dbrMYSZb_7Ew-Ti8lK9REe6kV3r1nEmLmpbiZL5eM5Q" />
+    <link rel="shortcut icon" type="image/png" href="img/fevicon.png" />
+
+    <!-- stylesheet -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/style.css">
+    <link rel="stylesheet" href="<?php echo base_url();?>assets/css/responsive.css">
+</head>
+<body>
+
+    <div class="body__wrap">
+        <div class="body__header">
+            <header class="container">
+                <div class="header__brand">
+                    <img src="assets/img/logo/logo-white.png" alt="">
+                </div>
+                <!-- end header brand -->
+                <nav class="header__nav">
+                    <ul>
+                        <li class="nav__list"><a href="" class="nav__link">Home</a></li>
+                    </ul>
+                </nav>
+                <!-- end header nav -->
+            </header>
+        </div>
+        <!-- end body header -->
