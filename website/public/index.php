97b046da498eb2a9e7b99a656d1911106742b02a
Rohit <rohitsraj12@gmail.com>
2021-11-09 16:10:00 +0530

    Create index.php

---
+++ website/public/index.php
@@ -0,0 +1,38 @@
+<?php
+
+    $page_title = "Home";
+    $page_meta_key = "";
+    $page_meta_description = "";
+
+
+    // config file
+    include('../private/config/url.config.php');
+    include('../private/required/header.inc.php');
+
+?>
+        <div class="body__banner">
+            <div class="wrap__banner">
+                <div class="banner__image">
+                    <div class="desktop__banner full__banner">
+                        <img src="" alt="">
+                    </div>
+                    <!-- end desktop banner -->
+                    <div class="mobile__banner full__banner">
+                        <img src="" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end body banner -->
+
+        <div class="body__container">
+            <main>
+                
+            </main>
+        </div>
+        <!-- end body container -->
+
+
+<?php
+    include('../private/required/footer.inc.php');
+?>
