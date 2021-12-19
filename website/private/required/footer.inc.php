16ac70a27cc21757d2634b6bd5fb9d4726fe8882
Rohit <rohitsraj12@gmail.com>
2021-12-19 18:33:18 +0530

    Update footer.inc.php

--- website/private/required/footer.inc.php
+++ website/private/required/footer.inc.php
@@ -1,9 +1,37 @@
-<div class="body__footer"></div>
+        <div class="body__footer" data-aos="fade-up"
+            data-aos-offset="100"
+            data-aos-delay="50"
+            data-aos-duration="1000"
+            data-aos-easing="ease-in-out">
+            <div class="container py-5">
+                <div class="row">
+                    <div class="col-md-3">
+                        <article>
+                            <p>
+                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed eum tempore modi quaerat. Aliquid, commodi! Earum consequuntur est molestiae maxime vitae repudiandae esse beatae! Accusantium ullam debitis consectetur illo fugit?
+                            </p>
+                        </article>
+                    </div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                    <div class="col-md-3"></div>
+                </div>
+            </div>
+        </div>
         <!-- end body footer -->
     </div>
     <!-- body wrap -->
 
     <!-- script -->
-    <script src=""></script>
+    <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>  
+    <script src="assets/js/script.js"></script>
+    <!-- load animation -->
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script>
+      AOS.init();
+    </script>
 </body>
 </html>
