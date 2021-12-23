7f67bec7086658cf7a6ae21ce160a443bb3e9249
Rohit <rohitsraj12@gmail.com>
2021-12-23 17:38:26 +0530

    Update footer.inc.php

--- website/private/required/footer.inc.php
+++ website/private/required/footer.inc.php
@@ -5,16 +5,161 @@
             data-aos-easing="ease-in-out">
             <div class="container py-5">
                 <div class="row">
-                    <div class="col-md-3">
+                    <div class="col-md-4">
                         <article>
-                            <p>
-                                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed eum tempore modi quaerat. Aliquid, commodi! Earum consequuntur est molestiae maxime vitae repudiandae esse beatae! Accusantium ullam debitis consectetur illo fugit?
-                            </p>
+                            <header class="footer__header">
+                                About Us
+                            </header>
+                            <figure class="footer__brand">
+                                <img src="<?php echo base_url();?>assets/img/logo/logo-white.png" alt="EDQI">
+                            </figure>
+                            <div class="footer__content pb-0">
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sed eum tempore modi quaerat. Aliquid, commodi! Earum consequuntur est molestiae maxime vitae repudiandae esse beatae! Accusantium ullam debitis consectetur illo fugit?
+                                </p>
+                            </div>
+                            <div class="footer__social-media">
+                                <ul>
+                                    <li class="footer__social-list">
+                                        <a href="" class="footer__social-link">
+                                            <i class="fa fa-facebook" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li class="footer__social-list">
+                                        <a href="" class="footer__social-link">
+                                            <i class="fa fa-instagram" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                    <li class="footer__social-list">
+                                        <a href="" class="footer__social-link">
+                                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
                         </article>
                     </div>
-                    <div class="col-md-3"></div>
-                    <div class="col-md-3"></div>
-                    <div class="col-md-3"></div>
+                    <div class="col-md-5">
+                        <article>
+                            <header class="footer__header">
+                                Quick Links
+                            </header>
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <div class="footer__content pb-0">
+                                        <div class="footer__links">
+                                            <ul>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        Home
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        About
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        FAQ
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        Contact Us
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="footer__content pb-0">
+                                        <div class="footer__links">
+                                            <ul>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        Web Development course
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        Java Course
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        Microsoft Course
+                                                    </a>
+                                                </li>
+                                                <li class="footer__list">
+                                                    <a href="<?php echo base_url();?>" class="footer__link">
+                                                        SAP coure
+                                                    </a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-md-3">
+                        <article>
+                            <header class="footer__header">
+                                Contact Details
+                            </header>
+                           
+                            <div class="footer__content pb-0">
+                                <div class="footer__links">
+                                    <ul>
+                                        <li class="footer__contact-list d-flex">
+                                            <div class="footer__contact-left">
+                                                <i class="fa fa-phone" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="footer__contact-right">
+                                                <a href="<?php echo base_url();?>" class="footer__contact-link">
+                                                   999 999 9999
+                                                </a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list d-flex">
+                                            <div class="footer__contact-left">
+                                                <i class="fa fa-envelope-o" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="footer__contact-right">
+                                                <a href="<?php echo base_url();?>" class="footer__contact-link">
+                                                   email
+                                                </a>
+                                            </div>
+                                        </li>
+                                        <li class="footer__contact-list d-flex">
+                                            <div class="footer__contact-left">
+                                                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                            </div>
+                                            <div class="footer__contact-right">
+                                                <a href="<?php echo base_url();?>" class="footer__contact-link">
+                                                   #21 bulding name,
+                                                   <br>
+                                                   area namespace asdasd
+                                                   <br>
+                                                   bangalore
+                                                </a>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+            <div class="copy__right">
+                <div class="container">
+                    <p>
+                        copy rights 2021 &copy; <a href="">edqi.in</a> | <a href="">Privacy Policy</a> | 
+                    </p>
                 </div>
             </div>
         </div>
