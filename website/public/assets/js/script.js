843fe1ac6a3c219cea73dc9995a99de6aab9d0d1
Rohit <rohitsraj12@gmail.com>
2021-12-30 00:11:04 +0530

    Update script.js

--- website/public/assets/js/script.js
+++ website/public/assets/js/script.js
@@ -1,5 +1,34 @@
-$(document).ready(function(){
-    $(".hamburger").click(function(){
-        $(".header__nav").slideToggle(400);
-    })
+$(document).ready(function(){    
+    const mediaQuery = window.innerWidth;
+
+    if(mediaQuery < 768){
+        // small desice and tabs
+        $(".hamburger").click(function(){
+            $(".header__nav").slideToggle(400);
+        })
+    } else {
+        // large device
+        $(window).scroll(function () {
+            var height = $(window).height();
+            var height = height / 4;
+            
+            // croll after 1/4 of screen on big screen 
+                // add fixed header class
+                // remove absolute header class
+                // give top 0 so just slide down effect 
+            if ($(window).scrollTop() > height) {
+                $(".body__header").removeClass("absolute__header");
+                $(".body__header").addClass("fixed__header");
+                $(".fixed__header").css({
+                    top: 0,
+                    transition: 'ease-in-out 300ms'
+                })
+                $(".sticky__enquiry").fadeIn();
+                // console.log(1000);
+            } else {
+                $(".body__header").removeClass("fixed__header");
+                $(".body__header").addClass("absolute__header");
+            }
+        });
+    }
 })
