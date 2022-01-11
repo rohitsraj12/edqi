4929ac779ef619106000f32a4b426a9ebc6d2020
Rohit <rohitsraj12@gmail.com>
2022-01-11 17:34:04 +0530

    Update about_us.php

--- website/public/about_us.php
+++ website/public/about_us.php
@@ -7,6 +7,13 @@
     include('../private/required/header.inc.php');
 
     // http://preview.themeforest.net/item/atena-college-university-and-campus-template/full_screen_preview/13090694?_ga=2.237159488.1949033100.1637125469-719680716.1604380046
+
+    // fonts: https://www.elegantthemes.com/blog/resources/elegant-icon-font
+
+    // rinita's template : https://preview.colorlib.com/#andrea
+        // http://eskimo.egemenerd.com/html/index.html?storefront=envato-elements
+
+
 ?>
 
     <div class="body__banner">
@@ -46,11 +53,11 @@
 
         <main>
             <section class="section__about-us py-5">
-                <div class="container">
+                <div class="container border-bottom">
                     <div class="row">
                         <div class="col-12">
                             <section>
-                                <header class="section__header text-center">
+                                <header class="section__header text-center py-5">
                                     <h2 class="primary-header">
                                         Some words about Atena
                                     </h2>
@@ -59,12 +66,12 @@
                                     </p>
                                 </header>
 
-                                <div class="row">
+                                <div class="row pb-5">
                                     <div class="col-md-8">
                                         <section class="section__about-left">
                                             <article>
                                                 <div class="section__body">
-                                                    <h2>
+                                                    <h2 class="secondary__header">
                                                         Founded
                                                     </h2>
                                                     <p>
@@ -73,9 +80,109 @@
                                                     <p>
                                                         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestiae accusantium est commodi tenetur ratione nesciunt, expedita voluptates dignissimos. Sed quibusdam nihil beatae eligendi? Laborum aut mollitia magni, minima nobis tempora!
                                                     </p>
+
+                                                    <ul class="d-sm-flex flex-sm-wrap found__points mb-3">
+                                                        <li class="w-50">Lorem, ipsum dolor. </li>
+                                                        <li class="w-50">Lorem, ipsum dolor.</li>
+                                                        <li class="w-50">Lorem ipsum dolor sit amet.</li>
+                                                        <li class="w-50">Lorem, ipsum dolor.</li>
+                                                        <li class="w-50">Lorem ipsum dolor sit amet.</li>
+                                                        <li class="w-50">Lorem ipsum dolor si.</li>
+                                                    </ul>
+
+                                                    <h2 class="secondary__header pt-3">
+                                                        EDQI name
+                                                    </h2>
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ratione expedita explicabo, veritatis alias ducimus molestias temporibus voluptas reiciendis dignissimos.
+                                                    </p>
+                                                    <h2 class="secondary__header pt-3">
+                                                        Founders and Director
+                                                    </h2>
+                                                    <p>
+                                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate ratione expedita explicabo, veritatis alias ducimus molestias temporibus voluptas reiciendis dignissimos.
+                                                    </p>
+
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <div class="faculty__wrap">
+                                                                <div class="left">
+                                                                    <figure>
+                                                                        <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                                    </figure>
+                                                                </div>
+                                                                <div class="right">
+                                                                    <h2>
+                                                                        Andy
+                                                                    </h2>
+                                                                    <p>
+                                                                        Director
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <div class="faculty__wrap">
+                                                                <div class="left">
+                                                                    <figure>
+                                                                        <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                                    </figure>
+                                                                </div>
+                                                                <div class="right">
+                                                                    <h2>
+                                                                        Andy
+                                                                    </h2>
+                                                                    <p>
+                                                                        Director
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <div class="faculty__wrap">
+                                                                <div class="left">
+                                                                    <figure>
+                                                                        <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                                    </figure>
+                                                                </div>
+                                                                <div class="right">
+                                                                    <h2>
+                                                                        Andy
+                                                                    </h2>
+                                                                    <p>
+                                                                        Director
+                                                                    </p>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </article>
                                         </section>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <div class="section__about-right p-3 border">
+                                            <section>
+                                                <article>
+                                                    <header class="secondary__header-sub ">
+                                                        Mission
+                                                    </header>
+                                                    <div class="section__about-right-body">
+                                                        <p>
+                                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsam neque error, enim cum doloremque aperiam iusto saepe dolorem natus?
+                                                        </p>
+
+                                                        <ul>
+                                                            <li><span class="num__circle">1</span> Lorem ipsum dolor sit amet.</li>
+                                                            <li><span class="num__circle">2</span> Lorem ipsum dolor sit amet consectetur.</li>
+                                                            <li><span class="num__circle">3</span> Lorem, ipsum dolor.</li>
+                                                            <li><span class="num__circle">4</span> Lorem ipsum dolor sit amet.</li>
+                                                            <li><span class="num__circle">5</span> Lorem ipsum dolor sit amet consectetur.</li>
+                                                        </ul>
+                                                    </div>
+                                                </article>
+                                            </section>
+                                        </div>
                                     </div>
                                 </div>
                             </section>
@@ -83,6 +190,66 @@
                     </div>
                 </div>
             </section>
+            <!-- end about us section -->
+
+            <section class="section__about-service pb-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <section>
+                                <article>
+                                    <figure class="border p-1">
+                                        <img class="w-100" src="<?php echo base_url();?>assets/img/service/01.jpg" alt="">
+                                    </figure>
+                                    <header>
+                                        <h1 class="secondary__header mb-0">
+                                            Service one
+                                        </h1> 
+                                    </header>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, nemo eveniet. Aspernatur placeat nihil non ab fuga, corrupti magni rerum.
+                                    </p>
+                                </article>
+                            </section>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <section>
+                                <article>
+                                    <figure class="border p-1">
+                                        <img class="w-100" src="<?php echo base_url();?>assets/img/service/01.jpg" alt="">
+                                    </figure>
+                                    <header>
+                                        <h1 class="secondary__header mb-0">
+                                            Service two
+                                        </h1> 
+                                    </header>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, nemo eveniet. Aspernatur placeat nihil non ab fuga, corrupti magni rerum.
+                                    </p>
+                                </article>
+                            </section>
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <section>
+                                <article>
+                                    <figure class="border p-1">
+                                        <img class="w-100" src="<?php echo base_url();?>assets/img/service/01.jpg" alt="">
+                                    </figure>
+                                    <header>
+                                        <h1 class="secondary__header mb-0">
+                                            Service three
+                                        </h1> 
+                                    </header>
+                                    <p>
+                                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum, nemo eveniet. Aspernatur placeat nihil non ab fuga, corrupti magni rerum.
+                                    </p>
+                                </article>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end section about service -->
         </main>
     </div>
     <!-- end body container -->
