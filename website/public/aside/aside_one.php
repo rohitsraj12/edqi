039d63e5500b9f1e3c1b81cca905376922985fcc
Rohit <rohitsraj12@gmail.com>
2022-01-12 17:15:47 +0530

    Create aside_one.php

---
+++ website/public/aside/aside_one.php
@@ -0,0 +1,89 @@
+<aside class="py-5">
+    <section class="section__promotion">
+        <article class="ads__one">
+            <figure>
+                <i class="fa fa-address-card-o" aria-hidden="true"></i>
+            </figure>
+            <header class="secondary__header text-light">
+                Any Ads
+            </header>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus ad sint ipsam molestias aliquid, repellat porro quia minima.
+            </p>
+            <footer>
+                <button class="btn btn-outline-light">
+                    Submit
+                </button>
+            </footer>
+        </article>
+        <!-- end ads one -->
+
+        <!-- <article class="ads__two">
+            <figure>
+                <i class="fa fa-address-card-o" aria-hidden="true"></i>
+            </figure>
+            <header class="secondary__header text-light">
+                Any Ads
+            </header>
+            <p>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus ad sint ipsam molestias aliquid, repellat porro quia minima.
+            </p>
+            <footer>
+                <button class="btn btn-outline-light">
+                    Submit
+                </button>
+            </footer>
+        </article> -->
+        <!-- end ads two -->
+
+        <article class="pb-3">
+            <header class="aside__header">
+                How to apply
+            </header>
+            <p class="aside__text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores perferendis aperiam soluta atque mollitia veniam vel perspiciatis! Repudiandae, accusantium harum?
+            </p>
+        </article>
+        <article class="adise__contact">
+            <header class="adise__contact-header d-flex justify-content-between pb-2">
+                <span class="left">By Phone</span> <span class="right"><i class="fa fa-phone" aria-hidden="true"></i></span>
+            </header>
+            <ul class="aside__text">
+                <li><a class="aside__link" href="tel:+919999999999">+91 999 999 9999</a></li>
+                <li class="short__text">Monday to Friday 9.00am - 6.00pm</li>
+            </ul>
+        </article>
+
+        <hr class="hr__line">
+
+        <article class="adise__contact">
+            <header class="adise__contact-header d-flex justify-content-between pb-2">
+                <span class="left">E-Mail</span> <span class="right"><i class="fa fa-envelope-o" aria-hidden="true"></i></span>
+            </header>
+            <ul class="aside__text">
+                <li><a class="aside__link " href="mailto:contact@mail.com">contact@mail.com</a></li>
+                <li class="short__text">We will contact you in 24 hours.</li>
+            </ul>
+        </article>
+
+        <hr class="hr__line">
+
+        <article class="adise__contact">
+            <header class="adise__contact-header d-flex justify-content-between pb-2">
+                <span class="left">Apply Online</span> <span class="right"><i class="fa fa-desktop" aria-hidden="true"></i></span>
+            </header>
+            
+                <p class="aside__text">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam vero quaerat amet autem atque aspernatur magnam perspiciatis iste doloremque ex veniam quam at aperiam vel inventore accusamus, reprehenderit suscipit et.
+                </p>
+            
+                <footer>
+                    <button class="btn btn__primary">
+                        Apply Now
+                    </button>
+                </footer>
+                
+        </article>
+
+    </section>
+</aside>
