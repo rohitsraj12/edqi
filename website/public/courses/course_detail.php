99a72885c759170b8cdd048d5cbeaccc75808abd
Rohit <rohitsraj12@gmail.com>
2022-01-12 17:16:15 +0530

    Create course_detail.php

---
+++ website/public/courses/course_detail.php
@@ -0,0 +1,237 @@
+<?php
+    $page_title = "Course detail";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    $course_name = "Web Development";
+    // config file
+    include('../../private/config/url.config.php');
+    include('../../private/required/header.inc.php');
+?>
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>courses/">
+                            Courses
+                        </a>
+                    </li>
+                    <li class="bread__crumb-parent">
+                        <?php echo $course_name;?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        
+        <main>
+            <div class="container">
+                <div class="row mt-4 pt-5">
+                    <div class="col-md-9">
+                        <header class="section__header mb-2" data-aos="fade-up"
+                                data-aos-offset="100"
+                                data-aos-delay="50"
+                                data-aos-duration="1000"
+                                data-aos-easing="ease-in-out">
+                            <h4 class="primary-header text-center">
+                                <?php echo $course_name;?>
+                            </h4>
+                            <!-- <p class="text-center section__sub-header">
+                                Cum doctus civibus efficiantur in imperdiet deterruisset.
+                            </p> -->
+                        </header>
+                        <!-- end section header -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-9">
+                        <section class="section__course-detail py-5">
+                            <div class="row">
+                                <div class="col-1">
+                                    <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article>
+                                        <header class="secondary__header">
+                                            Summery
+                                        </header>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima dolorem aspernatur, voluptatibus eum suscipit nisi doloribus unde in tempora debitis consectetur recusandae officia, quibusdam saepe nostrum ab quis quas aliquam! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Error eaque ab commodi neque soluta iure aliquid odio doloribus earum repellat.
+                                        </p>
+
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque quam deserunt unde qui illum soluta dolorem numquam, ratione rerum eos id culpa iusto! Nemo quaerat a soluta, voluptatem, consequatur id, nulla at dolor qui quis excepturi quia temporibus iste ipsa.
+                                        </p>
+
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, dignissimos. Corporis natus provident accusantium ab dolor in voluptatibus quasi ea similique autem aliquam inventore beatae reprehenderit repellendus impedit voluptatum earum non, neque dolorum aperiam fugit.
+                                        </p>
+
+                                        <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <figure>
+                                                    <img class="w-100" src="<?php echo base_url();?>assets/img/home/service_1.jpg" alt="">
+                                                </figure>
+                                                <p>
+                                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tempore, adipisci animi quod quam voluptatem. Corrupti quasi nisi explicabo possimus.
+                                                </p>
+                                            </div>
+                                            <div class="col-12 col-md-6">
+                                                <figure>
+                                                    <img class="w-100" src="<?php echo base_url();?>assets/img/home/service_2.jpg" alt="">
+                                                </figure>
+
+                                                <p>
+                                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic veritatis ut odio aliquid, unde quo expedita non?
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div> 
+
+                            <hr class="hr__line">
+                            
+                            <div class="row">
+                                <div class="col-1">
+                                    <i class="fa fa-book" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article>
+                                        <header class="secondary__header">
+                                            Lessons
+                                        </header>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nemo quam iste ab necessitatibus animi, corrupti deleniti magnam. Odit doloribus quidem harum repudiandae dolorem explicabo eaque voluptates quas reiciendis eum.
+                                        </p>
+                                        
+                                        <ul class="d-md-flex flex-wrap">
+                                            <li class="w-50">HTML</li>
+                                            <li class="w-50">CSS</li>
+                                            <li class="w-50">JavaScript</li>
+                                            <li class="w-50">JQuery</li>
+                                            <li class="w-50">PHP</li>
+                                            <li class="w-50">My SQL</li>
+                                            <li class="w-50">SEO</li>
+                                            <li class="w-50">Photoshop</li>
+                                        </ul>
+                                    </article>
+                                </div>
+                            </div>
+                            
+                            <hr class="hr__line">
+                            
+                            <div class="row">
+                                <div class="col-1">
+                                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article>
+                                        <header class="secondary__header">
+                                            Our Trainers
+                                        </header>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nemo quam iste ab necessitatibus animi, corrupti deleniti magnam. Odit doloribus quidem harum repudiandae dolorem explicabo eaque voluptates quas reiciendis eum.
+                                        </p>
+                                        
+                                        
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            12 Years exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            10 Years exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            7 Years Exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+
+                            <hr class="hr__line">
+
+                        </section>
+                    </div>
+                    <div class="col-12 col-md-3">
+                       <?php
+                            include("../aside/aside_one.php");
+                       ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../../private/required/footer.inc.php');
+?>
