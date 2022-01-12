15c8b8f7205757afc8a05b440f729285633b50ff
Rohit <rohitsraj12@gmail.com>
2022-01-12 23:05:21 +0530

    Create form.php

---
+++ website/public/modal/form.php
@@ -0,0 +1,34 @@
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+        <form action="" method="POST">
+            <div class="modal-header">
+                <h5 class="modal-title" id="staticBackdropLabel">Enquire Now</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="name" class="form-label">Name</label>
+                    <input type="text" class="form-control" id="name" aria-describedby="emailHelp" required>
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
+                <div class="mb-3">
+                    <label for="phone" class="form-label">Phone</label>
+                    <input type="tel" class="form-control" id="phone" aria-describedby="emailHelp" required>
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+                    <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="submit" class="btn btn__primary">Submit</button>
+            </div>
+        </form>
+    </div>
+  </div>
+</div>
