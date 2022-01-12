f15fbf6eb57e6c0f6ee7d8a418c3d22c434948f3
Rohit <rohitsraj12@gmail.com>
2022-01-12 17:16:22 +0530

    Create index.php

---
+++ website/public/services/index.php
@@ -0,0 +1,206 @@
+<?php
+    $page_title = "Edqi Services";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    // config file
+    include('../../private/config/url.config.php');
+    include('../../private/required/header.inc.php');
+?>
+
+
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="bread__crumb-parent">
+                        Services
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        <main>
+            <div class="container">
+                <div class="row mt-4 pt-5">
+                    <div class="col-md-12">
+                        <header class="section__header mb-2" data-aos="fade-up"
+                                data-aos-offset="100"
+                                data-aos-delay="50"
+                                data-aos-duration="1000"
+                                data-aos-easing="ease-in-out">
+                            <h4 class="primary-header text-center">
+                                Our Services 
+                            </h4>
+                            <p class="text-center section__sub-header">
+                                Cum doctus civibus efficiantur in imperdiet deterruisset.
+                            </p>
+                        </header>
+                    </div>
+                </div>
+                <div class="row">
+
+                    <div class="col-12 col-md-9">
+                        <section class="section__service py-5">
+                            <div class="row">
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                                <div class="col-12 col-sm-4 my-2">
+                                    <article class="article__service">
+                                        <figure>
+                                            <img class="w-100" src="<?php echo base_url();?>assets/img/course/01.jpg" alt="">
+                                        </figure>
+                                        <div class="article__center text-center">
+                                            <header>
+                                                <h2 class="secondary__header">
+                                                    Service 1
+                                                </h2>
+                                            </header>
+                                            <p>
+                                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi facilis soluta quibusdam doloribus vitae recusandae. Lorem ipsum dolor sit amet consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, cumque!
+                                            </p>
+                                        </div>
+                                        <footer class="text-center">
+                                            <a class="btn btn__outline-info" href="<?php echo base_url();?>courses/course_detail.php">Details</a>
+                                        </footer>
+                                    </article>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <div class="col-12 col-md-3">
+                       <?php
+                            include("../aside/aside_one.php");
+                       ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../../private/required/footer.inc.php');
+?>
