011a52b5e560eebf79cdaff701fa9826c96a6c50
Rohit <rohitsraj12@gmail.com>
2022-01-13 15:36:39 +0530

    Update footer.inc.php

--- website/private/required/footer.inc.php
+++ website/private/required/footer.inc.php
@@ -176,12 +176,48 @@
         <!-- end body footer -->
 
         <div class="section__fixed">
-            <div class="fixed__chat-boot">
-            <i class="fa fa-comment-o" aria-hidden="true"></i>
+            <div class="fixed__chat-boot"  data-bs-toggle="modal" data-bs-target="#enqiry-form">
+                <i class="fa fa-comment-o" aria-hidden="true"></i>
             </div>
         </div>
     </div>
     <!-- body wrap -->
+
+    
+    <!-- Modal -->
+    <div class="modal fade" id="enqiry-form" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form action="" method="POST">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="staticBackdropLabel">Enquire Now</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" class="form-control" id="name" aria-describedby="emailHelp" required>
+                            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                        </div>
+                        <div class="mb-3">
+                            <label for="phone" class="form-label">Phone</label>
+                            <input type="tel" class="form-control" id="phone" aria-describedby="emailHelp" required>
+                            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp" required>
+                            <!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn__primary">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <!-- end modal -->
 
     <!-- script -->
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
