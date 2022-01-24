d95cef7138b421ad1298fc644067a28fc081ba77
Rohit <rohitsraj12@gmail.com>
2022-01-24 23:47:07 +0530

    Create displayData.js

---
+++ website/public/assets/js/displayData.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $(".tel").text('+91 909-090-9090');
+    $(".tel").attr('href','tel:9090909090');
+
+    $(".mailto").text('info@edqi.in');
+    $(".mailto").attr('href','mailto:info@edqi.in');
+})
