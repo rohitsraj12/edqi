2267e3cbff6d8d2eb328a499ba9b7a70425545b7
Rohit <rohitsraj12@gmail.com>
2022-01-25 00:34:13 +0530

    Update footer.inc.php

--- website/private/required/footer.inc.php
+++ website/private/required/footer.inc.php
@@ -228,7 +228,7 @@
     <script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
     <!-- <script src="https://code.jquery.com/jquery-1.12.0.min.js"></script> -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script>  
+    <!-- <script src="<?php echo base_url();?>assets/js/owl.carousel.min.js"></script>   -->
     <script src="<?php echo base_url();?>assets/js/script.js"></script>
     <script src="<?php echo base_url();?>assets/js/displayData.js"></script>
     <!-- load animation -->
