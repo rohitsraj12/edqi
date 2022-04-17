f0d6af5d66b058314a7a464271a26c8163b6c8ea
Rohit <rohitsraj12@gmail.com>
2022-04-17 22:19:08 +0530

    Create training_and_global_certifications.php

---
+++ website/public/courses/training_and_global_certifications.php
@@ -0,0 +1,278 @@
+<?php
+    $page_title = "Training and global certifications | Global certifications";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    $course_name = "Training and global certifications";
+    // config file
+    include('../../private/config/url.config.php');
+    include('../../private/required/header.inc.php');
+?>
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us_sm.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container"data-aos="fade-in"
+        data-aos-offset="100"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>global_certifications/">
+                            Global Certifications
+                        </a>
+                    </li>
+                    <li class="bread__crumb-parent">
+                        <?php echo $course_name;?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        
+        <main>
+            <div class="container">
+                <div class="row mt-4 pt-5">
+                    <div class="col-md-9">
+                        <header class="section__header mb-2" data-aos="fade-up"
+                                data-aos-offset="100"
+                                data-aos-delay="50"
+                                data-aos-duration="1000"
+                                data-aos-easing="ease-in-out">
+                            <h4 class="primary-header text-center">
+                                <?php echo $course_name;?>
+                            </h4>
+                            <!-- <p class="text-center section__sub-header">
+                                Cum doctus civibus efficiantur in imperdiet deterruisset.
+                            </p> -->
+                        </header>
+                        <!-- end section header -->
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-12 col-md-9">
+                        <section class="section__course-detail py-5">
+                            <div class="row">
+                                <div class="col-1"data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                    <i class="fa fa-file-text-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                        <header class="secondary__header">
+                                            Summery
+                                        </header>
+                                        <p>
+                                            An Adobe Certification program is a course where an individual who has shown capability with Adobe Systems programming items by passing at least one item explicit capability tests set by Adobe. 
+                                        </p>
+
+                                        <!-- <div class="row">
+                                            <div class="col-12 col-md-6">
+                                                <figure>
+                                                    <img class="w-100" src="<?php echo base_url();?>assets/img/home/service_1.jpg" alt="">
+                                                </figure>
+                                                <p>
+                                                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sed tempore, adipisci animi quod quam voluptatem. Corrupti quasi nisi explicabo possimus.
+                                                </p>
+                                            </div>
+                                            <div class="col-12 col-md-6">
+                                                <figure>
+                                                    <img class="w-100" src="<?php echo base_url();?>assets/img/home/service_2.jpg" alt="">
+                                                </figure>
+
+                                                <p>
+                                                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic veritatis ut odio aliquid, unde quo expedita non?
+                                                </p>
+                                            </div>
+                                        </div> -->
+                                    </article>
+                                </div>
+                            </div> 
+
+                            <hr class="hr__line" data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                            
+                            <div class="row">
+                                <div class="col-1"data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                    <i class="fa fa-book" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                        <header class="secondary__header">
+                                            Lessons
+                                        </header>
+                                        <p>
+                                            <strong>Photoshop</strong> - Photoshop files have default file extension as .PSD, which stands for &quot;Photoshop Document&quot;. A PSD file stores an image with support for all features of Photoshop; these include layers with masks, transparency, text, alpha channels and spot colors, clipping paths, and duotone settings. This is in contrast to many other file formats (e.g., .JPG or .GIF) that restrict content to provide streamlined, predictable functionality. A PSD file has a maximum height and width of 30,000 pixels, and a length limit of two gigabytes.    
+                                        </p>
+                                        <p>
+                                            <strong>Illustrator</strong> - Adobe Illustrator is a vector graphics editor and design program developed and marketed by Adobe Inc. Originally designed for the Apple Macintosh, development of Adobe Illustrator began in 1985. Along with Creative Cloud (Adobe&#39;s shift to monthly or annual subscription service delivered over the Internet), Illustrator CC was released. The latest version, Illustrator 2022, was released on October 26, 2021, and is the 25th generation in the product line. Adobe Illustrator was reviewed as the best vector graphics editing program in 2018 by PC Magazine.
+                                        </p>
+                                        <p>
+                                           <strong>InDesign</strong> - Adobe InDesign is a desktop publishing and page layout designing software application produced by Adobe Inc. It can be used to create works such as posters, flyers, brochures, magazines, newspapers, presentations, books and ebooks. InDesign can also publish content suitable for tablet devices in conjunction with Adobe Digital Publishing Suite. Graphic designers and production artists are the principal users, creating and laying out periodical publications, posters, and print media.
+                                        </p>
+                                        <p>
+                                           <strong>After Effects</strong> - Adobe After Effects is a digital visual effects, motion graphics, and compositing application developed by Adobe Systems and used in the post-production process of film making, video games and television production. Among other things, After Effects can be used for keying, tracking, compositing, and animation. It also functions as a very basic non-linear editor, audio editor, and media transcoder. In 2019, the program won an Academy Award for scientific and technical achievement.
+                                        </p>
+                                        
+                                        <!-- <ul class="d-md-flex flex-wrap">
+                                            <li class="w-50">HTML</li>
+                                            <li class="w-50">CSS</li>
+                                            <li class="w-50">JavaScript</li>
+                                            <li class="w-50">JQuery</li>
+                                            <li class="w-50">PHP</li>
+                                            <li class="w-50">My SQL</li>
+                                            <li class="w-50">SEO</li>
+                                            <li class="w-50">Photoshop</li>
+                                        </ul> -->
+                                    </article>
+                                </div>
+                            </div>
+                            
+                            <hr class="hr__line" data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                            
+                            <div class="row">
+                                <div class="col-1"data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                    <i class="fa fa-user-o" aria-hidden="true"></i>
+                                </div>
+                                <div class="col-11">
+                                    <article data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                        <header class="secondary__header">
+                                            Our Trainers
+                                        </header>
+                                        <p>
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque nemo quam iste ab necessitatibus animi, corrupti deleniti magnam. Odit doloribus quidem harum repudiandae dolorem explicabo eaque voluptates quas reiciendis eum.
+                                        </p>
+                                        
+                                        
+                                        <div class="row">
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            12 Years exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            10 Years exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-4">
+                                                <div class="faculty__wrap">
+                                                    <div class="left">
+                                                        <figure>
+                                                            <img src="<?php echo base_url();?>assets/img/faculty/01.jpg" alt="">
+                                                        </figure>
+                                                    </div>
+                                                    <div class="right">
+                                                        <h2>
+                                                            Andy
+                                                        </h2>
+                                                        <p>
+                                                            7 Years Exp
+                                                        </p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+                            </div>
+
+                            <hr class="hr__line" data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+
+                        </section>
+                    </div>
+                    <div class="col-12 col-md-3">
+                       <?php
+                            include("../aside/aside_one.php");
+                       ?>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../../private/required/footer.inc.php');
+?>
