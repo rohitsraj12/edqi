7838f4c677c992325ea1cbd81cf6f6e4ff7210f6
Rohit <rohitsraj12@gmail.com>
2022-04-18 23:08:25 +0530

    Create collaboration_with_institutions.php

---
+++ website/public/academic_connect/collaboration_with_institutions.php
@@ -0,0 +1,196 @@
+<?php
+    $page_title = "Collaborations with Institutions | Global certifications";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    $course_name = "Collaborations with Institutions";
+    // config file
+    include('../../private/config/url.config.php');
+    include('../../private/required/header.inc.php');
+?>
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us_sm.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container"data-aos="fade-in"
+        data-aos-offset="100"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <!-- <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>global_certifications/">
+                            Global Certifications
+                        </a>
+                    </li> -->
+                    <li class="bread__crumb-parent">
+                        <?php echo $course_name;?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        <section class="section__about-us py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <section>
+                                <header class="section__header text-center py-5"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                    <h2 class="primary-header">
+                                        Collaborations with Institutions 
+                                    </h2>
+                                    <p class="section__sub-header">
+                                        Cum doctus civibus efficiantur in imperdiet deterruisset.
+                                    </p>
+                                </header>
+
+                                <div class="row pb-5">
+                                    <div class="col-md-3">
+                                        <div class="section__about-right"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section>
+                                                <article>
+                                                        <ul class="side__nav">
+                                                            <li><a href="#MoU’s">Details of MoU’s</a></li>
+                                                            <li><a href="#tie-ups">Details of tie-ups</a></li>
+                                                            <li><a href="#partners">Institutional partners</a></li>
+                                                            <li><a href="#">Activities conducted in Institutions</a></li>
+                                                        </ul>
+                                                </article>
+                                            </section>
+                                        </div>
+
+                                        <!-- <div class="section__about-right p-3 border bg-light"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section>
+                                                <article>
+                                                    
+                                                </article>
+                                            </section>
+                                        </div> -->
+                                    </div>
+                                    <div class="col-md-8">
+                                        <section class="section__about-left" id="MoU’s">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header">
+                                                            MOU with Dayananda Sagar Academy of Technology and Management.
+                                                        </h2>
+
+                                                        <ul class="">
+                                                            <li>1.	Faculty Exchange  </li>
+                                                            <li>2.	Research Support</li>
+                                                            <li>3.	Training Assistant</li>
+                                                            <li>4.	Product Support</li>
+                                                            <li>5.	Incubation support</li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </article>
+                                        </section>
+                                        <section class="section__about-left" id="tie-ups">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header pt-3">
+                                                            Partnership with REVA University UIIC 
+                                                        </h2>
+                                                        <p>
+                                                            Partnership with REVA University UIIC (University Industry Interaction center) for School of Computer Science and application and School of Civil Engineering. 
+                                                        </p>
+                                                        <ul class="pb-3">
+                                                            <li>
+                                                                1.	For providing training and Global certification 
+                                                            </li>
+                                                            <li>
+                                                                2.	Bootcamp in IOT
+                                                            </li>
+                                                            <li>
+                                                                3.	Internship Programs
+                                                            </li>
+                                                        </ul>
+                                                        <p>
+                                                            Registered with Certiport as Certiport Authorized Testing Center
+                                                        </p>
+                                                    </div>
+                                                    
+                                                  
+                                                </div>
+                                            </article>
+                                        </section>
+                                        <section class="section__about-left" id="partners">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header pt-3">
+                                                            Details of Institutional partners
+                                                        </h2>
+                                                       
+                                                        <ul class="d-sm-flex flex-sm-wrap found__points mb-3">
+                                                            <li class="w-50">REVA University  </li>
+                                                            <li class="w-50">Dayananda Sagar Academy of Technology and Management.</li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </article>
+                                        </section>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end about us section -->
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../../private/required/footer.inc.php');
+?>
