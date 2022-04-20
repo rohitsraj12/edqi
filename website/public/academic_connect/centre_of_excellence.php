7ebc77b44e662a86cb879926b614218e0b648b34
Rohit <rohitsraj12@gmail.com>
2022-04-20 20:27:24 +0530

    Create centre_of_excellence.php

---
+++ website/public/academic_connect/centre_of_excellence.php
@@ -0,0 +1,204 @@
+<?php
+    $page_title = "Centre of Excellence | Global certifications";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    $course_name = "Centre of Excellence";
+    // config file
+    include('../../private/config/url.config.php');
+    include('../../private/required/header.inc.php');
+?>
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us_sm.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container"data-aos="fade-in"
+        data-aos-offset="100"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <!-- <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>global_certifications/">
+                            Global Certifications
+                        </a>
+                    </li> -->
+                    <li class="bread__crumb-parent">
+                        <?php echo $course_name;?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        <section class="section__about-us py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <section>
+                                <header class="section__header text-center pt-5"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                    <h2 class="primary-header">
+                                    	Centre of Excellence 
+                                    </h2>
+                                    <p class="section__sub-header">
+                                        Cum doctus civibus efficiantur in imperdiet deterruisset.
+                                    </p>
+                                </header>
+
+                                <div class="row pb-5">
+                                    <div class="col-md-3">
+                                        <div class="section__about-right sticky-top pt-5"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section class="pt-3">
+                                                <article>
+                                                        <ul class="side__nav">
+                                                            <li><a href="#Objective-of-COE">Objective of COE</a></li>
+                                                            <li><a href="#Activities-conducted">Activities conducted</a></li>
+                                                            <li><a href="#Location-details">Location details</a></li>
+                                                        </ul>
+                                                </article>
+                                            </section>
+                                        </div>
+
+                                        <!-- <div class="section__about-right p-3 border bg-light"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section>
+                                                <article>
+                                                    
+                                                </article>
+                                            </section>
+                                        </div> -->
+                                    </div>
+                                    <div class="col-md-8 mt-3">
+                                        <section class="section__about-left pt-5" id="Objective-of-COE">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header">
+                                                            Objective of COE
+                                                        </h2>
+
+                                                        <ul class="">
+                                                            <li>1.	ET Platform  </li>
+                                                            <li>2.	Body of Knowledge</li>
+                                                            <li>3.	Communication</li>
+                                                            <li>4.	Collaboration</li>
+                                                            <li>5.	Area of Expertise</li>
+                                                            <li>6.	Research Funding</li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </article>
+                                        </section>
+                                        <section class="section__about-left pt-5" id="Activities-conducted">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header pt-3">
+                                                            Activities conducted 
+                                                        </h2>
+                                                        
+                                                        <ul class="">
+                                                            <li>
+                                                                1.	Academic support  
+                                                            </li>
+                                                            <li>
+                                                                2.	Feedback mechanism 
+                                                            </li>
+                                                            <li>
+                                                                3.	Placement and internships 
+                                                            </li>
+                                                            <li>
+                                                                4.	Dissertation work and publications   
+                                                            </li>
+                                                            <li>
+                                                                5.	Copyrights and IPR   
+                                                            </li>
+                                                            <li>
+                                                                6.	Termination and operation transfer 
+                                                            </li>
+                                                            <li>
+                                                                7.	Assets and liabilities 
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                    
+                                                  
+                                                </div>
+                                            </article>
+                                        </section>
+                                        <section class="section__about-left pt-5" id="Location-details">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <h2 class="secondary__header pt-3">
+                                                            Location details
+                                                        </h2>
+                                                       
+                                                        <ul>
+                                                            <li>
+                                                                1.	In Progress at Dayananda Sagar Academy of Technology and Management
+                                                            </li>
+                                                        </ul>
+                                                    </div>
+                                                </div>
+                                            </article>
+                                        </section>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end about us section -->
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../../private/required/footer.inc.php');
+?>
