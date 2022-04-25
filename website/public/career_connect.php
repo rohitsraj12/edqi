47ba242d6af5f6dd6050ef6b2220ba50e629908c
Rohit <rohitsraj12@gmail.com>
2022-04-25 20:31:48 +0530

    Create career_connect.php

---
+++ website/public/career_connect.php
@@ -0,0 +1,158 @@
+
+
+
+
+<?php
+    $page_title = "Career Connect";
+    $page_meta_key = "";
+    $page_meta_description = "";
+    $course_name = "Career Connect";
+    // config file
+    include('../private/config/url.config.php');
+    include('../private/required/header.inc.php');
+?>
+    <div class="body__banner">
+        <div class="wrap__banner">
+            <div class="banner__image">
+                <div class="desktop__banner other__banner" data-aos="fade-in"
+                    data-aos-offset="100"
+                    data-aos-delay="50"
+                    data-aos-duration="1000"
+                    data-aos-easing="ease-in">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us.jpg" alt="edqi bangalore">
+                </div>
+                <!-- end desktop banner -->
+                <div class="mobile__banner full__banner">
+                    <img src="<?php echo base_url();?>assets/img/banner/contact_us_sm.jpg" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end body banner -->
+
+    <div class="body__container"data-aos="fade-in"
+        data-aos-offset="100"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out">
+        <div class="bread__crumb bg-light py-2 border-bottom">
+            <div class="container">
+                <ul>
+                    <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>">
+                            <i class="fa fa-home" aria-hidden="true"></i>
+                        </a>
+                    </li>
+                    <!-- <li class="bread__crumb-parent">
+                        <a href="<?php echo base_url();?>global_certifications/">
+                            Global Certifications
+                        </a>
+                    </li> -->
+                    <li class="bread__crumb-parent">
+                        <?php echo $course_name;?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- end bread__crumb -->
+
+        <section class="section__about-us py-5">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12">
+                            <section>
+                                <header class="section__header text-center pb-5"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                    <h2 class="primary-header">
+                                        Career Connect
+                                    </h2>
+                                    <!-- <p class="section__sub-header">
+                                        Cum doctus civibus efficiantur in imperdiet deterruisset.
+                                    </p> -->
+                                </header>
+
+                                <div class="row pb-5">
+                                    <div class="col-md-3">
+                                        <!-- <div class="section__about-right sticky-top pt-5"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section class="pt-3">
+                                                <article>
+                                                        <ul class="side__nav">
+                                                            <li><a href="#MoU’s">Details of MoU’s</a></li>
+                                                            <li><a href="#tie-ups">Details of tie-ups</a></li>
+                                                            <li><a href="#partners">Institutional partners</a></li>
+                                                            <li><a href="#activities-conducted">Activities conducted in Institutions</a></li>
+                                                        </ul>
+                                                </article>
+                                            </section>
+                                        </div> -->
+
+                                        <!-- <div class="section__about-right p-3 border bg-light"  data-aos="fade-up"
+                                        data-aos-offset="100"
+                                        data-aos-delay="50"
+                                        data-aos-duration="1000"
+                                        data-aos-easing="ease-in-out">
+                                            <section>
+                                                <article>
+                                                    
+                                                </article>
+                                            </section>
+                                        </div> -->
+                                    </div>
+                                    <div class="col-md-12">
+                                        <section class="section__about-left" id="MoU’s">
+                                            <article>
+                                                <div class="section__body">
+                                                    <div  data-aos="fade-up"
+                                                        data-aos-offset="100"
+                                                        data-aos-delay="50"
+                                                        data-aos-duration="1000"
+                                                        data-aos-easing="ease-in-out">
+                                                        <!-- <h2 class="secondary__header text-center">
+                                                            MOU with Dayananda Sagar Academy of Technology and Management.
+                                                        </h2> -->
+                                                        <div class="row">
+                                                            <div class="col-sm-12">
+                                                                <div class="section__block pb-3">
+                                                                    <!-- <h2>
+                                                                        Details of MoU’s:
+                                                                    </h2>
+                                                                    <p>
+                                                                        A Memorandum of Understanding with Dayananda Sagar Academy of Technology and Management was signed for the following:
+                                                                    </p> -->
+                                                                    <ul class="found__points">
+                                                                        <li>
+                                                                            Resume builder – tool
+                                                                        </li>
+                                                                        <li>Career opportunities.</li>
+                                                                        <li>Please provide details regarding the career opportunities at Litheora and EDQI.</li>
+                                                                        
+                                                                    </ul>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </article>
+                                        </section>
+                                    </div>
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- end about us section -->
+        </main>
+    </div>
+    <!-- end body container -->
+
+<?php
+    include('../private/required/footer.inc.php');
+?>
