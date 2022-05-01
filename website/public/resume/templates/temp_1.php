881ea2d4ef2e67171cca00b9e60908cb45ad27d3
Rohit <rohitsraj12@gmail.com>
2022-05-01 23:44:51 +0530

    Update temp_1.php

--- website/public/resume/templates/temp_1.php
+++ website/public/resume/templates/temp_1.php
@@ -57,188 +57,185 @@
     </div> -->
 
     <!-- Main content -->
-      <section class="container resume border p-5">
-        <!-- title row -->
-          <div class="row">
+    <section class="container resume border p-5">
+      <!-- title row -->
+      <div class="row">
+      </div>
+      <!-- info row -->
+      <div class="row invoice-info">
+        <div class="col-sm-8 invoice-col">
+          <!-- <b>Invoice #007612</b><br> -->
+          <table>
+            <tr>
+              <th>Name</th>
+              <td>: Person Name</td>
+            </tr>
+            <tr>
+              <th>Phone Number</th>
+              <td>: +91 999 999 9999</td>
+            </tr>
+            <tr>
+              <th>Email</th>
+              <td>: mail@mail.com</td>
+            </tr>
+            <tr>
+              <th>Address</th>
+              <td>: #22, bld name, road name, mumbai 585858
+          </td>
+            </tr>
+          </table>
+        </div>
+        <!-- /.col -->
+      </div>
+      <!-- /.row -->
+      <hr>
+      <!-- Table row -->
+
+      <div class="object contianer py-4">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate libero incidunt similique cumque cum. Ipsum accusantium natus quia laborum consectetur, rem eligendi recusandae fuga laboriosam rerum soluta, vel repellat cumque odit quod aliquid minus voluptatum, deleniti temporibus nemo amet. Odit suscipit facilis nobis fuga nam natus sequi dolores voluptates atque?
+        </p>
+      </div>
+      <!-- end object -->
+
+      <div class="skill pb-4">
+        <h5>Skills</h5>
+        <div class="table-responsive">
+            <table class="table">
+              <tr>
+                <th style="width:15%">Coding:</th>
+                <td>: html, css, javaScript, jQuery, Bootstrap</td>
+              </tr>
+              <tr>
+                <th>Database</th>
+                <td>: mySql</td>
+              </tr>
+              <tr>
+                <th>Framework</th>
+                <td>: reactjs, Angular</td>
+              </tr>
+              <tr>
+                <th>Tools</th>
+                <td>: Visual Code, apache, Git, Photoshop</td>
+              </tr>
+            </table>
           </div>
-          <!-- info row -->
-          <div class="row invoice-info">
-            <div class="col-sm-8 invoice-col">
-              <!-- <b>Invoice #007612</b><br> -->
-              <table>
-                <tr>
-                  <th>Name</th>
-                  <td>: Person Name</td>
-                </tr>
-                <tr>
-                  <th>Phone Number</th>
-                  <td>: +91 999 999 9999</td>
-                </tr>
-                <tr>
-                  <th>Email</th>
-                  <td>: mail@mail.com</td>
-                </tr>
-                <tr>
-                  <th>Address</th>
-                  <td>: #22, bld name, road name, mumbai 585858
-              </td>
-                </tr>
-              </table>
+      </div>
+      <!-- end skils -->
+
+      <div class="experience">
+        <div class="pb-4">
+          <h5>Working Experience</h5>
+
+          <article>
+            <div class="pt-2 d-flex justify-content-between">
+              <div class="">
+                <h5 class="mb-0">
+                  Profile Name
+                </h5>
+                <p>
+                  company Name
+                </p>
+              </div>
+              <div class="">
+                <p>June 2020 - Jan 2022</p>
+              </div>
             </div>
-            <!-- /.col -->
-          </div>
-          <!-- /.row -->
-          <hr>
-          <!-- Table row -->
-
-          <div class="object contianer py-4">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate libero incidunt similique cumque cum. Ipsum accusantium natus quia laborum consectetur, rem eligendi recusandae fuga laboriosam rerum soluta, vel repellat cumque odit quod aliquid minus voluptatum, deleniti temporibus nemo amet. Odit suscipit facilis nobis fuga nam natus sequi dolores voluptates atque?
-            </p>
-          </div>
-          <!-- end object -->
-
-          <div class="skill pb-4">
-            <h5>Skills</h5>
             <div class="table-responsive">
-                <table class="table">
-                  <tr>
-                    <th style="width:15%">Coding:</th>
-                    <td>: html, css, javaScript, jQuery, Bootstrap</td>
-                  </tr>
-                  <tr>
-                    <th>Database</th>
-                    <td>: mySql</td>
-                  </tr>
-                  <tr>
-                    <th>Framework</th>
-                    <td>: reactjs, Angular</td>
-                  </tr>
-                  <tr>
-                    <th>Tools</th>
-                    <td>: Visual Code, apache, Git, Photoshop</td>
-                  </tr>
-                </table>
-              </div>
-          </div>
-          <!-- end skils -->
-
-          <div class="experience">
-            <div class="pb-4">
-              <h5>Working Experience</h5>
-
-              <article>
-                <div class="pt-2 d-flex justify-content-between">
-                  <div class="">
-                    <h5 class="mb-0">
-                      Profile Name
-                    </h5>
-                    <p>
-                      company Name
-                    </p>
-                  </div>
-                  <div class="">
-                    <p>June 2020 - Jan 2022</p>
-                  </div>
-                </div>
-                <div class="table-responsive">
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolore repellat quae, modi magnam consequatur facere pariatur illo incidunt iste similique quidem ut qui error doloremque maiores nobis, vel voluptatem.
-                  </p>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iste vel ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
-                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, obcaecati! Corrupti repellat quasi iusto nihil quod dolores? </li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam voluptates nobis similique magnam, neque suscipit quaerat autem quas maiores, asperiores mollitia et! </li>
-                    <li>Lorem ipsum dolor sit amet ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
-                  </ul>  
-                </div>
-                <!-- exp one -->
-              </article>
-              
-              <article>
-                <div class="pt-2 d-flex justify-content-between">
-                  <div class="">
-                    <h5 class="mb-0">
-                      Profile Name
-                    </h5>
-                    <p>
-                      company Name
-                    </p>
-                  </div>
-                  <div class="">
-                    <p>June 2020 - Jan 2022</p>
-                  </div>
-                </div>
-                <div class="table-responsive">
-                  <p>
-                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolore repellat quae, modi magnam consequatur facere pariatur illo incidunt iste similique quidem ut qui error doloremque maiores nobis, vel voluptatem.
-                  </p>
-                  <ul>
-                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iste vel ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
-                    <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, obcaecati! Corrupti repellat quasi iusto nihil quod dolores? </li>
-                    <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam voluptates nobis similique magnam, neque suscipit quaerat autem quas maiores, asperiores mollitia et! </li>
-                    <li>Lorem ipsum dolor sit amet ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
-                  </ul>  
-                </div>
-                <!-- exp one -->
-              </article>
-              
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolore repellat quae, modi magnam consequatur facere pariatur illo incidunt iste similique quidem ut qui error doloremque maiores nobis, vel voluptatem.
+              </p>
+              <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iste vel ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, obcaecati! Corrupti repellat quasi iusto nihil quod dolores? </li>
+                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam voluptates nobis similique magnam, neque suscipit quaerat autem quas maiores, asperiores mollitia et! </li>
+                <li>Lorem ipsum dolor sit amet ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
+              </ul>  
             </div>
-          </div>
-          <!-- end experience -->
+            <!-- exp one -->
+          </article>
           
-          <div class="academic">
-            <h5>Academic</h5>
-            <div class="row">
-              
-              <div class="col-xs-12 table-responsive">
-                <table class="table table-striped">
-                  <thead>
-                  <tr>
-                    <th width="10%">Year</th>
-                    <th width="40%">Course</th>
-                    <th width="40%">Univercity</th>
-                    <th width="10%">Grade</th>
-                  </tr>
-                  </thead>
-                  <tbody>
-                  <tr>
-                    <td>2011</td>
-                    <td>B.Tech</td>
-                    <td>IIT Mumbai</td>
-                    <td>A</td>
-                  </tr>
-                  <tr>
-                    <td>2009</td>
-                    <td>A Levels</td>
-                    <td>Mumbai Univercity</td>
-                    <td>A+</td>
-                  </tr>
-
-                  <tr>
-                    <td>2011</td>
-                    <td>B.Tech</td>
-                    <td>IIT Mumbai</td>
-                    <td>A</td>
-                  </tr>
-                  <tr>
-                    <td>2009</td>
-                    <td>A Levels</td>
-                    <td>Mumbai Univercity</td>
-                    <td>A+</td>
-                  </tr>
-                  </tbody>
-                </table>
+          <article>
+            <div class="pt-2 d-flex justify-content-between">
+              <div class="">
+                <h5 class="mb-0">
+                  Profile Name
+                </h5>
+                <p>
+                  company Name
+                </p>
               </div>
-              <!-- /.col -->
+              <div class="">
+                <p>June 2020 - Jan 2022</p>
+              </div>
             </div>
-            <!-- /.row -->
+            <div class="table-responsive">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolore repellat quae, modi magnam consequatur facere pariatur illo incidunt iste similique quidem ut qui error doloremque maiores nobis, vel voluptatem.
+              </p>
+              <ul>
+                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia iste vel ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
+                <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis, obcaecati! Corrupti repellat quasi iusto nihil quod dolores? </li>
+                <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam voluptates nobis similique magnam, neque suscipit quaerat autem quas maiores, asperiores mollitia et! </li>
+                <li>Lorem ipsum dolor sit amet ipsam facere ipsa, soluta eius! Ullam minus iste deleniti. </li>
+              </ul>  
+            </div>
+            <!-- exp one -->
+          </article>
+          
+        </div>
+      </div>
+      <!-- end experience -->
+      
+      <div class="academic">
+        <h5>Academic</h5>
+        <div class="row">
+          
+          <div class="col-xs-12 table-responsive">
+            <table class="table table-striped">
+              <thead>
+              <tr>
+                <th width="10%">Year</th>
+                <th width="40%">Course</th>
+                <th width="40%">Univercity</th>
+                <th width="10%">Grade</th>
+              </tr>
+              </thead>
+              <tbody>
+              <tr>
+                <td>2011</td>
+                <td>B.Tech</td>
+                <td>IIT Mumbai</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>2009</td>
+                <td>A Levels</td>
+                <td>Mumbai Univercity</td>
+                <td>A+</td>
+              </tr>
 
+              <tr>
+                <td>2011</td>
+                <td>B.Tech</td>
+                <td>IIT Mumbai</td>
+                <td>A</td>
+              </tr>
+              <tr>
+                <td>2009</td>
+                <td>A Levels</td>
+                <td>Mumbai Univercity</td>
+                <td>A+</td>
+              </tr>
+              </tbody>
+            </table>
           </div>
-          <!-- academic -->
+          <!-- /.col -->
+        </div>
+        <!-- /.row -->
 
-          <!-- this row will not appear when printing -->
-      <!-- </div> -->
+      </div>
+      <!-- academic -->
     </section>
     <!-- /.content -->
   </div>
