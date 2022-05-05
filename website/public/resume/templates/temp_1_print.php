46e09fb2a1b6663d1158777741df7a18c5d00ff8
Rohit <rohitsraj12@gmail.com>
2022-05-05 22:57:53 +0530

    Update temp_1_print.php

--- website/public/resume/templates/temp_1_print.php
+++ website/public/resume/templates/temp_1_print.php
@@ -11,20 +11,15 @@
     $db = $_POST['database'];
     $framework = $_POST['framework'];
     $tool = $_POST['tool'];
-
-
   }
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Resume Builder</title>
+  <title>EDQI - Resume Builder</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
